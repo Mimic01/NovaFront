ae9d1b08e23afe379061c577cc97650e59888b8e
Alex <alex.escudero.m@outlook.com>
2022-01-24 12:34:28 -0400

    CatsVsDogs Convolutional NN with data augmentation and preprocessing examples.

---
+++ Misc/ConvCatsvsDogs.py
@@ -0,0 +1,306 @@
+import os, shutil
+from keras import layers
+from keras import models
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.python.keras.applications.vgg16 import VGG16
+from tensorflow.keras import optimizers
+
+original_dataset_dir = 'J:\Downloads\DogsVsCats\\train'
+base_dir = 'J:\Downloads\DogsVsCats\cats_and_dogs_small'
+#os.mkdir(base_dir)
+
+train_dir = os.path.join(base_dir, 'train')
+#os.mkdir(train_dir)
+validation_dir = os.path.join(base_dir, 'validation')
+#os.mkdir(validation_dir)
+test_dir = os.path.join(base_dir, 'test')
+#os.mkdir(test_dir)
+
+train_cats_dir = os.path.join(train_dir, 'cats')
+#os.mkdir(train_cats_dir)
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+#os.mkdir(train_dogs_dir)
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+#os.mkdir(validation_cats_dir)
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+#os.mkdir(validation_dogs_dir)
+test_cats_dir = os.path.join(test_dir, 'cats')
+#os.mkdir(test_cats_dir)
+test_dogs_dir = os.path.join(test_dir, 'dogs')
+#os.mkdir(test_dogs_dir)
+
+# fnames = ['cat.{}.jpg'.format(i) for i in range(1000)]
+# for fname in fnames:
+#     src = os.path.join(original_dataset_dir, fname)
+#     dst = os.path.join(train_cats_dir, fname)
+#     shutil.copyfile(src, dst)
+#
+# fnames = ['cat.{}.jpg'.format(i) for i in range(1000, 1500)]
+# for fname in fnames:
+#     src = os.path.join(original_dataset_dir, fname)
+#     dst = os.path.join(validation_cats_dir, fname)
+#     shutil.copyfile(src, dst)
+#
+# fnames = ['cat.{}.jpg'.format(i) for i in range(1500, 2000)]
+# for fname in fnames:
+#     src = os.path.join(original_dataset_dir, fname)
+#     dst = os.path.join(test_cats_dir, fname)
+#     shutil.copyfile(src, dst)
+#
+# fnames = ['dog.{}.jpg'.format(i) for i in range(1000)]
+# for fname in fnames:
+#     src = os.path.join(original_dataset_dir, fname)
+#     dst = os.path.join(train_dogs_dir, fname)
+#     shutil.copyfile(src, dst)
+#
+# fnames = ['dog.{}.jpg'.format(i) for i in range(1000, 1500)]
+# for fname in fnames:
+#     src = os.path.join(original_dataset_dir, fname)
+#     dst = os.path.join(validation_dogs_dir, fname)
+#     shutil.copyfile(src, dst)
+#
+# fnames = ['dog.{}.jpg'.format(i) for i in range(1500, 2000)]
+# for fname in fnames:
+#     src = os.path.join(original_dataset_dir, fname)
+#     dst = os.path.join(test_dogs_dir, fname)
+#     shutil.copyfile(src, dst)
+
+print('total training cat images:', len(os.listdir(train_cats_dir)))
+print('total training dog images:', len(os.listdir(train_dogs_dir)))
+print('total validation cat images:', len(os.listdir(validation_cats_dir)))
+print('total validation dog images:', len(os.listdir(validation_dogs_dir)))
+print('total test cats images:', len(os.listdir(test_cats_dir)))
+print('total test dog images:', len(os.listdir(test_dogs_dir)))
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.summary()
+
+model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4), metrics=['acc'])
+
+train_datagen = ImageDataGenerator(rescale=1. / 255)
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(train_dir, target_size=(150, 150), batch_size=20,
+                                                    class_mode='binary')
+validation_generator = test_datagen.flow_from_directory(validation_dir, target_size=(150, 150), batch_size=20,
+                                                        class_mode='binary')
+
+# Fitting with generator use steps_per_epoch to delimit the epoch, here batches are 20 samples, so it will take
+# 100 batches until you see your target of 2,000 samples.
+history = model.fit_generator(train_generator, steps_per_epoch=100, epochs=30, validation_data=validation_generator,
+                              validation_steps=50)
+model.save('cats_and_dogs_small_1.h5')
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title("Training and validation accuracy")
+plt.legend()
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
+
+# We need data augmentation!
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(learning_rate=1e-4), metrics=['acc'])
+
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+# validation data should not be augmented
+train_generator = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(train_dir, target_size=(150, 150), batch_size=32,
+                                                    class_mode='binary')
+validation_generator = test_datagen.flow_from_directory(validation_dir, target_size=(150, 150), batch_size=32,
+                                                        class_mode='binary')
+history = model.fit_generator(train_generator, steps_per_epoch=100, epochs=100, validation_data=validation_generator,
+                              validation_steps=50)
+model.save('cats_and_dogs_small_2.h5')
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title("Training and validation accuracy")
+plt.legend()
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
+
+# Extracting features using a pretrained convolutional base
+# Use ImageDataGenerator to extract images as numpy arrays as well as their labels
+
+conv_base = VGG16(weights='imagenet', include_top=False, input_shape=(150, 150, 3))
+
+datagen = ImageDataGenerator(rescale=1. / 255)
+batch_size = 20
+
+
+def extract_features(directory, sample_count):
+    features = np.zeros(shape=(sample_count, 4, 4, 512))
+    labels = np.zeros(shape=(sample_count))
+    generator = datagen.flow_from_directory(directory, target_size=(150, 150), batch_size=batch_size,
+                                            class_mode='binary')
+    i = 0
+    for inputs_batch, labels_batch in generator:
+        features_batch = conv_base.predict(inputs_batch)
+        features[i * batch_size: (i + 1) * batch_size] = features_batch
+        labels[i * batch_size: (i + 1) * batch_size] = labels_batch
+        i += 1
+        if i * batch_size >= sample_count:
+            break
+    return features, labels
+
+
+train_features, train_labels = extract_features(train_dir, 2000)
+validation_features, validation_labels = extract_features(validation_dir, 1000)
+test_features, test_labels = extract_features(test_dir, 1000)
+
+# Extracted features are currently of shape (samples, 4, 4, 512), you'll feed them to a densely connected classifier
+# so first you must flatten them to (samples, 8192):
+train_features = np.reshape(train_features, (2000, 4 * 4 * 512))
+validation_features = np.reshape(validation_features, (1000, 4 * 4 * 512))
+test_features = np.reshape(test_features, (1000, 4 * 4 * 512))
+
+model = models.Sequential()
+model.add(layers.Dense(256, activation='relu', input_dim=4 * 4 * 512))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(optimizer=optimizers.RMSprop(lr=2e-5), loss='binary_crossentropy', metrics=['acc'])
+history = model.fit(train_features, train_labels, epochs=30, batch_size=20,
+                    validation_data=(validation_features, validation_labels))
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+
+# Feature extraction with data augmentation
+
+model = models.Sequential()
+model.add(conv_base)
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+# Remember to freeze the convolutional network before compiling the model
+conv_base.trainable = False
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest')
+test_datagen = ImageDataGenerator(rescale=1./255)
+train_generator = train_datagen.flow_from_directory(train_dir, target_size=(150, 150), batch_size=20,
+                                                    class_mode='binary')
+validation_generator = test_datagen.flow_from_directory(validation_dir, target_size=(150, 150), batch_size=20,
+                                                        class_mode='binary')
+model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=2e-5), metrics=['acc'])
+history = model.fit_generator(
+    train_generator, steps_per_epoch=100, epochs=30, validation_data=validation_generator, validation_steps=50)
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+
+# Fine tuning - Freezing all layers up to a specific one
+conv_base.trainable = True
+set_trainable = False
+for layer in conv_base.layers:
+    if layer.name == 'block5_conv1':
+        set_trainable = True
+    if set_trainable:
+        layer.trainable = True
+    else:
+        layer.trainable = False
+model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-5), metrics=['acc'])
+history = model.fit_generator(train_generator, steps_per_epoch=100, epochs=100, validation_data=validation_generator,
+                              validation_steps=50)
+test_generator = test_datagen.flow_from_directory(test_dir, target_size=(150, 150), batch_size=20, class_mode='binary')
+test_loss, test_acc = model.evaluate_generator(test_generator, steps=50)
+print('test acc:', test_acc)
---
+++ Misc/ConvnetVisualization.py
@@ -0,0 +1,129 @@
+from keras.models import load_model
+from keras.preprocessing import image
+import numpy as np
+import matplotlib.pyplot as plt
+from keras import models
+from tensorflow.python.keras.applications.vgg16 import VGG16
+from keras import backend as K
+
+# Visualize intermediate activations
+model = load_model('cats_and_dogs_small_2.h5')
+model.summary()
+img_path = ''
+img = image.load_img(img_path, target_size=(150, 150))
+img_tensor = image.img_to_array(img)
+img_tensor = np.expand_dims(img_tensor, axis=0)
+img_tensor /= 255.
+print(img_tensor.shape)
+
+plt.imshow(img_tensor[0])
+plt.show()
+
+layer_outputs = [layer.output for layer in model.layers[:8]]
+activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
+activations = activation_model.predict(img_tensor)
+first_layer_activation = activations[0]
+print(first_layer_activation.shape)
+# Visualize the fourth channel
+plt.matshow(first_layer_activation[0, :, :, 4], cmap='viridis')
+# Visualize the seventh channel
+plt.matshow(first_layer_activation[0, :, :, 7], cmap='viridis')
+# Visualize every channel in every intermediate activation
+layer_names = []
+for layer in model.layers[:8]:
+    layer_names.append(layer.name)
+images_per_row = 16
+for layer_name, layer_activation in zip(layer_names, activations):
+    n_features = layer_activation.shape[-1]
+    size = layer_activation.shape[1]
+    n_cols = n_features // images_per_row
+    display_grid = np.zeros((size * n_cols, images_per_row * size))
+
+    for col in range(n_cols):
+        for row in range(images_per_row):
+            channel_image = layer_activation[0, :, :, col * images_per_row + row]
+            channel_image -= channel_image.mean()
+            channel_image /= channel_image.std()
+            channel_image *= 64
+            channel_image += 128
+            channel_image = np.clip(channel_image, 0, 255).astype('uint8')
+            display_grid[col * size: (col + 1) * size, row * size: (row + 1) * size] = channel_image
+    scale = 1. / size
+    plt.figure(figsize=(scale * display_grid.shape[1], scale * display_grid.shape[0]))
+    plt.title(layer_name)
+    plt.grid(False)
+    plt.imshow(display_grid, aspect='auto', cmap='viridis')
+
+# Visualize convnet filters
+
+model = VGG16(weights='imagenet', include_top=False)
+layer_name = 'block3_conv1'
+filter_index = 0
+layer_output = model.get_layer(layer_name).output
+loss = K.mean(layer_output[:, :, :, filter_index])
+grads = K.gradients(loss, model.input)[0]
+grads /= (K.sqrt(K.mean(K.square(grads))) + 1e-5)
+
+iterate = K.function([model.input], [loss, grads])
+loss_value, grads_value = iterate([np.zeros((1, 150, 150, 3))])
+
+input_img_data = np.random.random((1, 150, 150, 3)) * 20 + 128
+step = 1
+for i in range(40):
+    loss_value, grads_value = iterate([input_img_data])
+    input_img_data += grads_value * step
+
+
+def deprocess_image(x):
+    x -= x.mean()
+    x /= (x.std() + 1e-5)
+    x *= 0.1
+    x += 0.5
+    x = np.clip(x, 0, 1)
+    x *= 255
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
+
+
+# Function to generate filter visualizations
+def generate_pattern(layer_name, filter_index, size=150):
+    layer_output = model.get_layer(layer_name).output
+    loss = K.mean(layer_output[:, :, :, filter_index])
+    grads = K.gradients(loss, model.input)[0]
+    grads /= (K.sqrt(K.mean(K.square(grads))) + 1e-5)
+    iterate = K.function([model.input], [loss, grads])
+    input_img_data = np.random.random((1, size, size, 3)) * 20 + 128.
+
+    step = 1
+    for i in range(40):
+        loss_value, grads_value = iterate([input_img_data])
+        input_img_data += grads_value * step
+    img = input_img_data[0]
+    return deprocess_image(img)
+
+
+# Pattern that the zeroth channel in layer block3_conv1 responds to maximally
+plt.imshow(generate_pattern('block3_conv1', 0))
+
+# Generating a grid of all filter response patterns in a layer
+layer_name = 'block1_conv1'
+size = 64
+margin = 5
+
+results = np.zeros((8 * size + 7 * margin, 8 * size + 7 * margin, 3))
+for i in range(8):
+    for j in range(8):
+        filter_img = generate_pattern(layer_name, i + (j * 8), size=size)
+
+        horizontal_start = i * size + i * margin
+        horizontal_end = horizontal_start + size
+        vertical_start = j * size + j * margin
+        vertical_end = vertical_start + size
+        results[horizontal_start: horizontal_end, vertical_start: vertical_end, :] = filter_img
+plt.figure(figsize=(20, 20))
+plt.imshow(results)
+
+
+
+# Visualizing heatmaps of class activation
+

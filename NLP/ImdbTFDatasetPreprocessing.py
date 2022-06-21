d48c5d39156b2eed96015c90b687f29bb739084b
Alex <alex.escudero.m@outlook.com>
2022-06-21 16:55:11 -0400

    some changes

--- NLP/ImdbTFDatasetPreprocessing.py
+++ NLP/ImdbTFDatasetPreprocessing.py
@@ -7,24 +7,18 @@ BATCH_SIZE = 64
 VOCAB_SIZE = 10000
 MAX_SEQUENCE_LENGTH = 250
 
-data, ds_info = tfds.load('ag_news_subset', with_info=True, batch_size=BATCH_SIZE, shuffle_files=True,
-                          as_supervised=True)
-train_ds, test_ds = data['train'], data['test']
-for i, data in enumerate(train_ds.take(3)):
-    print(i + 1, data[0].shape, data[1])
+train_ds, ds_info = tfds.load('imdb_reviews', split='train[:80%]', batch_size=BATCH_SIZE, shuffle_files=True,
+                              as_supervised=True, with_info=True)
+val_ds = tfds.load('imdb_reviews', split='train[80%:]', batch_size=BATCH_SIZE, shuffle_files=True, as_supervised=True)
 
-for review_batch, label_batch in test_ds.take(1):
+for review_batch, label_batch in val_ds.take(1):
     for i in range(5):
         print("Review: ", review_batch[i].numpy())
         print("Label: ", label_batch[i].numpy())
 
-EPOCHS = 20
-NUM_CLASSES = ds_info.features["label"].num_classes
-train_size = len(train_ds)
-batch_size = 64
-
 vectorize_layer = tf.keras.layers.TextVectorization(max_tokens=VOCAB_SIZE, output_mode='int',
                                                     output_sequence_length=MAX_SEQUENCE_LENGTH)
+# Make a text-only dataset (without labels), then call `TextVectorization.adapt`.
 train_text = train_ds.map(lambda text, labels: text)
 vectorize_layer.adapt(train_text)
 
@@ -35,7 +29,7 @@ def vectorize_text(text, label):
 
 
 train_ds = train_ds.map(vectorize_text)
-test_ds = test_ds.map(vectorize_text)
+val_ds = val_ds.map(vectorize_text)
 
 AUTOTUNE = tf.data.AUTOTUNE
 
@@ -45,7 +39,7 @@ def configure_dataset(dataset):
 
 
 train_ds = configure_dataset(train_ds)
-test_ds = configure_dataset(test_ds)
+val_ds = configure_dataset(val_ds)
 
 
 def create_model(vocab_size, num_labels):
@@ -58,13 +52,12 @@ def create_model(vocab_size, num_labels):
     return model
 
 
-model = create_model(vocab_size=VOCAB_SIZE + 1, num_labels=NUM_CLASSES)
-model.compile(loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True), optimizer='adam',
-              metrics=['sparse_categorical_accuracy'])
+model = create_model(vocab_size=VOCAB_SIZE + 1, num_labels=1)
 print(model.summary())
-history = model.fit(train_ds, validation_data=test_ds, epochs=3)
+model.compile(loss=tf.losses.BinaryCrossentropy(from_logits=True), optimizer='adam', metrics=['accuracy'])
+history = model.fit(train_ds, validation_data=val_ds, epochs=3)
 
-loss, accuracy = model.evaluate(test_ds)
+loss, accuracy = model.evaluate(val_ds)
 
 print("Loss: ", loss)
 print("Accuracy: {:2.2%}".format(accuracy))
